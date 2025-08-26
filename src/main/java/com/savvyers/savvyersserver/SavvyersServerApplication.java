0fd2ee6bc1192c97958eac6cdc5ffde61f529dda
gimmicks-u <is@pusan.ac.kr>
2025-08-26 18:21:01 +0900

    init: init project

---
+++ src/main/java/com/savvyers/savvyersserver/SavvyersServerApplication.java
@@ -0,0 +1,13 @@
+package com.savvyers.savvyersserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavvyersServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SavvyersServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savvyers/savvyersserver/SavvyersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savvyers.savvyersserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavvyersServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
